bca4c482e82fdc078356d670d3575154bbb3ae6b
Wolfgang Barth <barth.wolf@gmail.com>
2016-06-24 16:21:29 +1000

    registered missing resources for datatables

--- parabank/__init__.py
+++ parabank/__init__.py
@@ -1,12 +1,22 @@
 from pyramid.config import Configurator
 
 # we must make sure custom models are known at database initialization!
-from parabank import models
+from parabank08 import models
+
+from models import Syncretism, Pattern, Paradigm, Word
+from interfaces import ISyncretism, IPattern, IParadigm
+from clld import interfaces as clldInt
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('clldmpg')
+    config.include('clld.web.app')
+
+    config.register_resource('syncretism', Syncretism, ISyncretism, with_index=True)
+    config.register_resource('pattern', Pattern, IPattern, with_index=True)
+    config.register_resource('paradigm', Paradigm, IParadigm, with_index=True)
+    config.register_resource('word', Word, clldInt.IValue, with_index=True)
+
     return config.make_wsgi_app()
