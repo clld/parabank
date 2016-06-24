3a36e75cc3a079e65d9ce8a2c13d23fd7f79d4ff
Wolfgang Barth <barth.wolf@gmail.com>
2016-06-24 16:20:28 +1000

    interfaces of custom tables

---
+++ parabank/interfaces.py
@@ -0,0 +1,10 @@
+from zope.interface import Interface
+
+class ISyncretism(Interface):
+    """marker"""
+
+class IPattern(Interface):
+    """marker"""
+
+class IParadigm(Interface):
+    """marker"""
