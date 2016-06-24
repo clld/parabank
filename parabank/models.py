4d8727b48f73d071fc5662d1d79611037250b1b3
Wolfgang Barth <barth.wolf@gmail.com>
2016-06-24 16:17:20 +1000

    Add files via upload

--- parabank/models.py
+++ parabank/models.py
@@ -1,25 +1,145 @@
 from zope.interface import implementer
 from sqlalchemy import (
     Column,
-    String,
     Unicode,
+    String,
     Integer,
-    Boolean,
     ForeignKey,
-    UniqueConstraint,
-)
+    UniqueConstraint
+    )
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.ext.hybrid import hybrid_property
 
 from clld import interfaces
 from clld.db.meta import Base, CustomModelMixin
-#from clld.db.models.common import Language
+from clld.db.models.common import Value, ValueSet, Parameter,  IdNameDescriptionMixin, HasSourceMixin, Contribution, Language, Unit
+
+from parabank08 import interfaces as parabank_interfaces
 
 
-#-----------------------------------------------------------------------------
+class ParadigmLanguage(Base):
+    """association table for many-to-many lookup between Paradigm and Language"""
+    __table_args__ = (UniqueConstraint('paradigm_pk', 'language_pk'),)
+    paradigm_pk = Column(Integer, ForeignKey('paradigm.pk'), primary_key=True)
+    language_pk = Column(Integer, ForeignKey('language.pk'), primary_key=True)
+
+
+class PatternSyncretism(Base):
+    """association table for many-to-many lookup between Pattern and Syncretism"""
+    __table_args__ = (UniqueConstraint('pattern_pk', 'syncretism_pk'),)
+    pattern_pk = Column(Integer, ForeignKey('pattern.pk'), primary_key=True)
+    syncretism_pk = Column(Integer, ForeignKey('syncretism.pk'), primary_key=True)
+
+
+class ParameterParadigm(Base):
+    """association table for many-to-many lookup between Parameter and Paradigm"""
+    parameter_pk = Column(Integer, ForeignKey('parameter.pk'))
+    paradigm_pk = Column(Integer, ForeignKey('paradigm.pk'))
+
+
+# -----------------------------------------------------------------------------
 # specialized common mapper classes
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+
 #@implementer(interfaces.ILanguage)
-#class parabankLanguage(CustomModelMixin, Language):
+#class ParabankLanguage(CustomModelMixin, Language):
+#    """language used as given by CLLD +
+#       many-to-many with Paradigm adds backref column: paradigms"""
 #    pk = Column(Integer, ForeignKey('language.pk'), primary_key=True)
+
+#@implementer(interfaces.IContribution)
+#class ParabankContribution(CustomModelMixin, Contribution):
+#    """conrtibution used as given by CLLD"""
+#    pk = Column(Integer, ForeignKey('contribution.pk'), primary_key=True)
+
+
+@implementer(interfaces.IValue)
+class Word(CustomModelMixin, Value):
+    """label on each valueSet
+       one-to-many relation to parabankValueSet adds backref column = valuesets"""
+    pk = Column(Integer, ForeignKey('value.pk'), primary_key=True)
+    word_name = Column(Unicode, default=True)
+    word_ipa = Column(Unicode)
+    word_description = Column(Unicode)
+    sound = Column(Unicode)
+
+
+@implementer(interfaces.IParameter)
+class ParabankParameter(CustomModelMixin, Parameter):
+    """one-to-many relation to parabankValueSet adds backref column = valuesets
+       many-to-many relation to Paradigm adds backref column = paradigms"""
+    pk = Column(Integer, ForeignKey('parameter.pk'), primary_key=True)
+    # parameter_name = Column(Unicode, default=True) # Inherited from Base class
+    # parameter_abbreviation = Column(Unicode) # we don't need it!
+    # parameter_description = Column(Unicode) # Inherited from Base class
+
+
+@implementer(parabank_interfaces.ISyncretism)
+class Syncretism(IdNameDescriptionMixin, Base):
+    """one-to-many relation to parabankValueSet adds backref column = valuesets
+       many-to-many relation to Pattern adds backref column = patterns"""
+    pk = Column(Integer, primary_key=True)  # Inherited from Base class
+    name = Column(Unicode, default=True) # Inherited from Base class
+    description = Column(Unicode) # Inherited from Base class
+
+
+@implementer(parabank_interfaces.IPattern)
+class Pattern(IdNameDescriptionMixin, Base):
+    """one-to-many relation to parabankValueSet adds backref column = valuesets
+       many-to-many relation to Syncretism"""
+    #pk = Column(Unicode, primary_key=True) # Inherited from Base class
+    #name = Column(Unicode, default=True) # Inherited from Base class
+    #description = Column(Unicode)  # change to pattern_description # Inherited from Base class
+    #syncretisms = relationship('Syncretism', secondary=PatternSyncretism.__table__, backref='patterns')
+
+
+@implementer(interfaces.IValueSet)
+class ParabankValueSet(CustomModelMixin, ValueSet):
+    """core model for lookup
+       many-to-one relation to Word
+       many-to-one relation to Language
+       many-to-one relation to Parameter
+       many-to-one relation to Syncretism
+       many-to-one relation to Pattern
+       """
+    pk = Column(Integer, ForeignKey('valueset.pk'), primary_key=True)
+
+    # many-to-one with Syncretism
+    syncretism_pk = Column(String, ForeignKey('syncretism.pk'), default=True)
+    syncretism = relationship("Syncretism", backref="all_valuesets")
+
+    # many-to-one with Pattern
+    pattern_pk = Column(Unicode, ForeignKey('pattern.pk'), default=True)
+    pattern = relationship("Pattern", backref="all_valuesets")
+
+    # many-to-one with Word covered by clld.models.value
+    # word_pk = Column(Integer, ForeignKey('word.pk'))
+    # word = relationship("Word", backref="all_valuesets")
+
+    # many-to-one with Language covered by clld.models.valueset
+    # language_pk = Column(Integer, ForeignKey('parabanklanguage.pk'), default=True)
+    # language = relationship("parabankLanguage", backref="all_valuesets")
+
+    # many-to-one with Parameter covered by clld.models.valueset
+    # parameter_pk = Column(Integer, ForeignKey('parabankparameter.pk'), default=True)
+    # parameter = relationship("parabankParameter", backref="all_valuesets")
+
+    # many-to-one with parabankContribution covered by clld.models.valueset
+    # contribution_pk = Column(Integer, ForeignKey('parabankcontribution.pk'))
+    # contribution = relationship("parabankContribution", backref="all_valuesets")
+
+    # source = Column(Unicode)
+
+
+@implementer(parabank_interfaces.IParadigm)
+class Paradigm(IdNameDescriptionMixin, Base):
+    """many-to-many relation to parabankLanguages
+       many-to-many relation to parabankParameter"""
+    pk = Column(Integer, primary_key=True)
+    id = Column(Unicode)
+    name = Column(Unicode, default=True)
+    description = Column(Unicode)
+    languages = relationship('Language', secondary=ParadigmLanguage.__table__, backref='paradigms')
+    parameters = relationship('ParabankParameter', secondary=ParameterParadigm.__table__, backref='paradigms')
+
+    # __mapper_args__ = {'polymorphic_identity': 'paradigm',}
