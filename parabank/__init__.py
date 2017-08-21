b2b9a0a91a4059358e55eec752c7cc07de9e62d4
xrotwang <xrotwang@googlemail.com>
2017-08-21 17:43:50 +0200

    color-code by language family on parameter maps as well

--- parabank/__init__.py
+++ parabank/__init__.py
@@ -3,14 +3,24 @@ from __future__ import unicode_literals, print_function, division
 
 from pyramid.config import Configurator
 from clld_glottologfamily_plugin.util import LanguageByFamilyMapMarker
-from clld.interfaces import IMapMarker
+from clld.interfaces import IValue, IValueSet, IMapMarker
+
+from clld.web.app import get_adapters
 
 # we must make sure custom models are known at database initialization!
 from parabank import models
 
 from parabank.models import Syncretism, Pattern, Paradigm, Word
 from parabank.interfaces import ISyncretism, IPattern, IParadigm
-from clld import interfaces as clldInt
+
+
+class ParabankMapMarker(LanguageByFamilyMapMarker):
+    def get_icon(self, ctx, req):
+        if IValue.providedBy(ctx):
+            return LanguageByFamilyMapMarker.get_icon(self, ctx.valueset.language, req)
+        if IValueSet.providedBy(ctx):
+            return LanguageByFamilyMapMarker.get_icon(self, ctx.language, req)
+        return LanguageByFamilyMapMarker.get_icon(self, ctx, req)
 
 
 def main(global_config, **settings):
@@ -19,7 +29,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('clld.web.app')
     config.include('clld_glottologfamily_plugin')
-    config.registry.registerUtility(LanguageByFamilyMapMarker(), IMapMarker)
+    config.registry.registerUtility(ParabankMapMarker(), IMapMarker)
 
     config.register_resource('syncretism', Syncretism, ISyncretism, with_index=True)
     config.register_resource('pattern', Pattern, IPattern, with_index=True)
